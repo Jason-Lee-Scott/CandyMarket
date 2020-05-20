9d03ca880a973e363d814762788b45124fbbae53
Kelsey Creel <kelseycreel@gmail.com>
2020-05-20 18:36:31 -0500

    setup complete

---
+++ CandyMarket/CandyMarket/Models/OwnersCandy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyMarket.Models
+{
+    public class OwnersCandy
+    {
+        public int ID { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int UserId { get; set; }
+        public int CandyId { get; set; }
+        public bool IsEaten { get; set; }
+        public DateTime EatenDate { get; set; }
+    }
+}
